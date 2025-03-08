af92dd65fb2e9c422b44cef389c574a7dabd3466
Daniel Mouris <dgmouris@gmail.com>
2025-03-08 15:44:39 -0700

    Added way to load players automatically

--- frontend/components/poiu/POIUnitCard.js
+++ frontend/components/poiu/POIUnitCard.js
@@ -14,7 +14,6 @@ export default function POIUnitCard({title, team}) {
     "Unit: 5"
   ]
 
-
   return <Card className="border-t-8">
     <CardHeader
       className="flex flex-col items-center justify-between pb-2"
@@ -46,42 +45,22 @@ export default function POIUnitCard({title, team}) {
       {/* forwards handle if it's 4 */}
       <div className="flex items-center w-full justify-between gap-2">
         <PlayerProfile
-          playerName={"connor mcdavid"}
-          number={97}
-          position={"Center"}
-          imageUrl={"https://assets.nhle.com/mugs/nhl/20242025/EDM/8478402.png"}
-          badgeColor={"blue"}
+          playerId={8475786}
         />
         <PlayerProfile
-          playerName={"connor mcdavid"}
-          number={97}
-          position={"Center"}
-          imageUrl={"https://assets.nhle.com/mugs/nhl/20242025/EDM/8478402.png"}
-          badgeColor={"blue"}
+          playerId={8478402}
         />
         <PlayerProfile
-          playerName={"connor mcdavid"}
-          number={97}
-          position={"Center"}
-          imageUrl={"https://assets.nhle.com/mugs/nhl/20242025/EDM/8478402.png"}
-          badgeColor={"blue"}
+          playerId={8479318}
         />
       </div>
       {/* defensemen */}
       <div className="flex items-center w-3/5 justify-between gap-2">
         <PlayerProfile
-          playerName={"connor mcdavid"}
-          number={97}
-          position={"Center"}
-          imageUrl={"https://assets.nhle.com/mugs/nhl/20242025/EDM/8478402.png"}
-          badgeColor={"blue"}
+          playerId={8480803}
         />
         <PlayerProfile
-          playerName={"connor mcdavid"}
-          number={97}
-          position={"Center"}
-          imageUrl={"https://assets.nhle.com/mugs/nhl/20242025/EDM/8478402.png"}
-          badgeColor={"blue"}
+          playerId={8475218}
         />
       </div>
     </CardHeader>
