99bdef6cd4a24a7fdc98373766b42db626570d99
Daniel Mouris <dgmouris@gmail.com>
2025-03-08 14:16:43 -0700

    Added Select for POIUs

--- frontend/components/poiu/POIUnitCard.js
+++ frontend/components/poiu/POIUnitCard.js
@@ -1,19 +1,47 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 import PlayerProfile from "../player/PlayerProfile"
 
 
 export default function POIUnitCard({title, team}) {
 
+  const DUMMY_UNITS = [
+    "Unit: 1",
+    "Unit: 2",
+    "Unit: 3",
+    "Unit: 4",
+    "Unit: 5"
+  ]
+
+
   return <Card className="border-t-8">
     <CardHeader
       className="flex flex-col items-center justify-between pb-2"
-
     >
-      <div className="text-center">
+      <div className="flex w-full mb-2">
+        <div className="text-center flex-1">
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{team}</CardDescription>
+        </div>
+        {/* Units will have to be changed a bit differently */}
+        <Select className="flex-none" value={""} onValueChange={()=>{}}>
+          <SelectTrigger className="w-[200px]">
+            <SelectValue placeholder="Select different POIU" />
+          </SelectTrigger>
+          <SelectContent>
+            {DUMMY_UNITS.map((dummyUnit, index)=> {
 
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{team}</CardDescription>
+               return <SelectItem key={dummyUnit} value={dummyUnit}>
+                <div className="flex items-center gap-2">
+                  <div className="w-3 h-3 rounded-full" />
+                  {dummyUnit}
+                </div>
+              </SelectItem>
+            })}
+          </SelectContent>
+        </Select>
+
       </div>
       {/* forwards handle if it's 4 */}
       <div className="flex items-center w-full justify-between gap-2">
@@ -57,6 +85,5 @@ export default function POIUnitCard({title, team}) {
         />
       </div>
     </CardHeader>
-
   </Card>
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,10 +5,8 @@ import POIUnitCard from "@/components/poiu/POIUnitCard"
 export default function Home() {
   return (
     <div className="container mx-auto py-6 px-4">
-      <h1 className="text-3xl font-bold text-center mb-8">NHL Player On Ice Unit Comparison Tool</h1>
-
+      <h1 className="text-3xl font-bold text-center mb-8">NHL Player On Ice Unit (POIU) Comparison Tool</h1>
       <PlayerSearchInput />
-
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <POIUnitCard
           title={`Connor Mcdavid's most common POIU`}
@@ -19,7 +17,6 @@ export default function Home() {
           team={"Toronto Maple Leafs"}
         />
       </div>
-
     </div>
   );
 }
