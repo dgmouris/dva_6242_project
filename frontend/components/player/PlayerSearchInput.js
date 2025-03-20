17cf5fd6ef85e79d49fadb3ae28da81689686434
Daniel Mouris <dgmouris@gmail.com>
2025-03-20 16:08:16 -0600

    Added search

--- frontend/components/player/PlayerSearchInput.js
+++ frontend/components/player/PlayerSearchInput.js
@@ -1,14 +1,52 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
+import PlayerProfile from './PlayerProfile'
+
+import { searchPlayers } from '@/utils/api/players'
+
+
+
+import {
+  useQuery,
+} from '@tanstack/react-query'
+
 // maybe this should be in a
 export default function PlayerSearchInput () {
   const [searchQuery, setSearchQuery] = useState("")
+  const [searchResults, setSearchResults] = useState([])
 
-  const handleSearch = () => {
+  let { isPending, error, data, refetch } = useQuery({
+    queryKey: [`search-results`],
+    queryFn: async ()=> {
+      return await searchPlayers({name: searchQuery})
+    },
+    enabled: searchQuery.length >= 3,
+    staleTime: Infinity,
+  })
 
+  const handleSearch = async () => {
+
+  }
+  useEffect(()=> {
+    if (searchQuery.length >= 3) {
+      // const data = await searchPlayers({name: searchQuery})
+      refetch()
+      console.log(data)
+      if (data) {
+        setSearchResults(data.players)
+      }
+      // this is from the api backend
+    } else {
+      setSearchResults([])
+    }
+
+  }, [searchQuery])
+
+  if (isPending) {
+    return "Loading ..."
   }
 
   return <div className="w-full max-w-3xl mx-auto p-4">
@@ -27,6 +65,18 @@ export default function PlayerSearchInput () {
           />
         </div>
         <Button onClick={handleSearch}>Search</Button>
+      </div>
+      <div className="space-y-3">
+        {
+          searchResults.map((player)=> {
+            console.log(player)
+            return <PlayerProfile
+              key={player.id}
+              isSearchResult={true}
+              playerId={player.id}
+            />
+          })
+        }
       </div>
     </div>
   </div>
