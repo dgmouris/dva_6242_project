d7f5b74496d05871f7fe282d67ee3e8e0a730bf6
Daniel Mouris <dgmouris@gmail.com>
2025-03-20 16:13:45 -0600

    Removed buggy ui line

--- frontend/components/player/PlayerSearchInput.js
+++ frontend/components/player/PlayerSearchInput.js
@@ -45,10 +45,6 @@ export default function PlayerSearchInput () {
 
   }, [searchQuery])
 
-  if (isPending) {
-    return "Loading ..."
-  }
-
   return <div className="w-full max-w-3xl mx-auto p-4">
     <div className="flex flex-col space-y-4">
       <h1 className="text-2xl font-bold">NHL Player Search</h1>
